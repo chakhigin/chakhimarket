db7432bcd9fb18802fc39fd001dfddf9be0e217c
chakhigin <ehsankhforex@gmail.com>
2025-01-01 21:33:15 +0330

    fixed images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains:["localhost"]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'chakhimarket.netlify.app',
+        port: '',
+        pathname: '/public/images/',
+        search: '',
+      },
+    ],
   },
 };
 
